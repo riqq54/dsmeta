cbb0bc8efd2047bf064f3257a2b79d2c902f97db
riqq54 <henriquefreitasl28@gmai.com>
2022-07-13 19:22:23 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return(
+    <>
     <h1>Olá!</h1>
+    <NotificationButton/>
+    </>
   )
 }
 
---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/img/notification-icon.svg'
+
+import './styles.css'
+
+function NotificationButton() {
+    return (
+            <div className="botao-notificar">
+                <img src={icon} alt="Notificar"/>
+            </div>
+    )
+}
+
+export default NotificationButton
